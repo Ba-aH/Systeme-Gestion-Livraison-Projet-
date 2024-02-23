bdab8f235fa13ad1a98acfdca3d39c317f7ae982
Ferchichirami <ramiferchichi32@gmail.com>
2024-02-23 21:31:25 +0100

    form admin work

--- src/Controller/AdministratorController.php
+++ src/Controller/AdministratorController.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ClientRepository;
-
-
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 #[Route('/administrator')]
 class AdministratorController extends AbstractController
 {
@@ -27,5 +27,39 @@ class AdministratorController extends AbstractController
         ]);
         
     }
+    #[Route('/submit_form', name: 'submit_form', methods: ['POST'])]
+    public function submit_form(ClientRepository $clientRepository,Request $request ,EntityManagerInterface $entityManager): Response
+    {
+        $id = $request->request->get('id');
+        $user = $entityManager->getRepository(Client::class)->findOneBy(['id' => $id]);
+
+     
+            // Update the Livraison entity with the provided data
+
+            $user->setEmail($request->request->get('email'));
+            $user->setUsername($request->request->get('username'));
+            $user->setPassword($request->request->get('password'));
+            $user->setPhone($request->request->get('phone'));
+            $user->setNom($request->request->get('nom'));
+            $user->setPrenom($request->request->get('prenom'));
+          
+           
+ 
+            // Persist the changes to the database
+            $entityManager->flush();
+            $entityManager->flush();
+            $clients=$clientRepository->findAll();
+          
+            // Redirect or render a response as needed
+            return $this->render('administrator/coursier_list.html.twig', [
+              
+                'clients' => $clients
+            ]);
+       
+
+
+
+       
     
+}
 }
