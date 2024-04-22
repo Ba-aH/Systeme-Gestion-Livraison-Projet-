dcce0d4ebbfd061db0c17cdff9b193701bee32a1
Ferchichirami <ramiferchichi32@gmail.com>
2024-04-22 15:40:21 +0100

    admin suivie tours temchi dra kifeh

--- migrations/Version20240422104847.php
+++ migrations/Version20240422104847.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240421111301 extends AbstractMigration
+final class Version20240422104847 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,11 +21,15 @@ final class Version20240421111301 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE contact_us (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, sujet VARCHAR(255) NOT NULL, message VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE notification (id INT AUTO_INCREMENT NOT NULL, livraison_id INT DEFAULT NULL, note VARCHAR(255) DEFAULT NULL, INDEX IDX_BF5476CA8E54FB25 (livraison_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE notification ADD CONSTRAINT FK_BF5476CA8E54FB25 FOREIGN KEY (livraison_id) REFERENCES livraison (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE notification DROP FOREIGN KEY FK_BF5476CA8E54FB25');
         $this->addSql('DROP TABLE contact_us');
+        $this->addSql('DROP TABLE notification');
     }
 }
